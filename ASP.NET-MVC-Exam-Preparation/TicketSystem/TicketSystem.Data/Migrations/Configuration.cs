95904607c6138c1aac11d893cca7ae4fd9193434
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-06-11 21:47:58 +0300

    Migrations added;

---
+++ ASP.NET-MVC-Exam-Preparation/TicketSystem/TicketSystem.Data/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+namespace TicketSystem.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TicketSystemDbContext>
+    {
+        public Configuration()
+        {
+            this.AutomaticMigrationsEnabled = true;
+            this.AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(TicketSystem.Data.TicketSystemDbContext context)
+        {
+            
+        }
+    }
+}
