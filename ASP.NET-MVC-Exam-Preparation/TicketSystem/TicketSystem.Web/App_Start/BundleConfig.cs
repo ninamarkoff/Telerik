88ea664e007c459db377ee031ec2000758eb0f30
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-06-12 17:46:36 +0300

    Kendo installed;

--- ASP.NET-MVC-Exam-Preparation/TicketSystem/TicketSystem.Web/App_Start/BundleConfig.cs
+++ ASP.NET-MVC-Exam-Preparation/TicketSystem/TicketSystem.Web/App_Start/BundleConfig.cs
@@ -8,8 +8,37 @@ namespace TicketSystem.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
+
+            RegisterScriptBundles(bundles);
+            RegisterContentBundles(bundles);
+
+            BundleTable.EnableOptimizations = false;    
+        }
+
+        private static void RegisterContentBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                        "~/Content/kendo/kendo.common.min.css",
+                        "~/Content/kendo/kendo.common-bootstrap.min.css",
+                        "~/Content/kendo/kendo.silver.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/custom").Include(
+                      "~/Content/site.css"));
+        }
+
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                        "~/Scripts/kendo/kendo.web.min.js",
+                        "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        ("~/Scripts/kendo/jquery.min.js")));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,10 +51,6 @@ namespace TicketSystem.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
