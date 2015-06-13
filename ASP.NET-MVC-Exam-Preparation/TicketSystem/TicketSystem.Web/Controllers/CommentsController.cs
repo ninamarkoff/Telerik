c56dc5be951b75ec454252e4512fb8df85c6aa37
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-06-13 22:23:26 +0300

    CommentsController.cs /absent in last commit/;

---
+++ ASP.NET-MVC-Exam-Preparation/TicketSystem/TicketSystem.Web/Controllers/CommentsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TicketSystem.Data;
+using TicketSystem.Models;
+using TicketSystem.Web.ViewModels.Comments;
+
+namespace TicketSystem.Web.Controllers
+{
+    public class CommentsController : BaseController
+    {
+        public CommentsController(ITicketSystemData data)
+            :base(data)
+        {
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PostComment(PostCommentViewModel comment)
+        {
+            if (comment!=null && ModelState.IsValid)
+            {
+                var dbComment = Mapper.Map<Comment>(comment);
+                dbComment.Author = this.UserProfile;
+                var ticket = this.Data.Tickets.GetById(comment.TicketId);
+                if (ticket == null)
+                {
+                    throw new HttpException(404, "Ticket not found");
+                }
+
+                ticket.Comments.Add(dbComment);
+                this.Data.SaveChanges();
+
+                var viewModel = Mapper.Map<CommentViewModel>(dbComment);
+
+                return PartialView("_CommentPartial", viewModel);
+            }
+
+            throw new HttpException(400, "Invalid comment");
+        }
+    }
+}
