1915945a5d35056c9b4a16ec8b081c41fec75647
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-06-28 00:24:20 +0300

    Lecture_4_Console_Input_Output

---
+++ C# Part 1/Lecture_4_Console_Input_Output/Problem_01_SumOfThreeInts/SumOfThreeInts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/* Problem 1, Lecture 4:
+ * Write a program that reads 3 integer numbers from the console and prints their sum. */
+
+public class SumOfThreeInts
+{
+    public static void Main()
+    {
+        int num1, num2, num3;
+        bool isNum1Correct, isNum2Correct, isNum3Correct;
+
+        isNum1Correct = int.TryParse(Console.ReadLine(), out num1);
+        isNum2Correct = int.TryParse(Console.ReadLine(), out num2);
+        isNum3Correct = int.TryParse(Console.ReadLine(), out num3);
+
+        if (isNum1Correct && isNum2Correct && isNum3Correct)
+        {
+            Console.WriteLine("The sum of the three integers is {0}", num1 + num2 + num3);
+        }
+        else
+        {
+            Console.WriteLine("The input data is not in correct format");
+        }
+        
+    }
+}
+
---
+++ C# Part 1/Lecture_4_Console_Input_Output/Problem_02_Circle/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/* Problem 2, Lecture 4:
+ * Write a program that reads the radius r of a circle and prints its perimeter and area. */
+
+
+public class Circle
+{
+    public static void Main()
+    {
+        double circleRadius;
+        bool isCircleRadiusCorrect = double.TryParse(Console.ReadLine(), out circleRadius);
+        Console.WriteLine("The circle with radius {0} has perimeter={1} and area={2}", 
+            circleRadius, 2*Math.PI*circleRadius, Math.PI*circleRadius*circleRadius);
+
+    }
+}
+
